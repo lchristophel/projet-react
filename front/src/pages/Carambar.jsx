6ef48b14049ee330fb8fd95607775bfc2944dbd5
Christophe <grebert62231@gmail.com>
2022-05-10 14:40:25 +0200

    install + router + header

---
+++ front/src/pages/Carambar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+const Carambar = () => {
+    return ( 
+      <div>
+        <h1>J'aime les carambars !</h1>
+      </div>
+     );
+  }
+   
+  export default Carambar;
---
+++ front/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "../components/Header";
+const Home = () => {
+    return ( 
+      <div>
+        <Header />
+        <h1>page Home</h1>
+      </div>
+     );
+  }
+   
+  export default Home;
